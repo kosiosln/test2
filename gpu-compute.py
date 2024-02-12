3b923e1814ae3f96e97071538510275b5ed95a86
kosiosln <kosiosln@gmail.com>
2024-02-12 12:48:09 +0200

    Update gpu-compute.py

--- gpu-compute.py
+++ gpu-compute.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from web3 import Web3
+from eth_account import Account
 
 def generate_random_private_key(prefix):
     key_length = 32  # Private key length in bytes
@@ -10,19 +10,19 @@ def generate_random_private_key(prefix):
             return prefix + random_suffix
 
 def find_matching_address(prefixes, private_key):
-    web3 = Web3(Web3.HTTPProvider('http://localhost:8545'))  # Connect to local Ethereum node
-    address = web3.eth.account.privateKeyToAccount(private_key).address.lower()
+    address = Account.from_key(private_key).address.lower()
     return any(address.startswith(prefix.lower()) for prefix in prefixes)
 
 try:
     while True:
         random_private_key = generate_random_private_key("9ffb3894434556451e2254800ecae2f99cfe964b1440901aaf718ec1")
-        prefixes_to_check = ["0x2b6"]
+        derived_address = Account.from_key(random_private_key).address.lower()
+        prefixes_to_check = ["0x2b6ed"]
         if find_matching_address(prefixes_to_check, random_private_key):
             # Command to set GPU for computing
-            command = f'nvidia-smi -i 0,1,2,3 -c EXCLUSIVE_PROCESS'
+            command = 'nvidia-smi -i 0,1,2,3 -c EXCLUSIVE_PROCESS'
             subprocess.run(command, shell=True, check=True)
-            print(f"Random Private Key: {random_private_key}, "
-                  f"Matching Address: {address}")
+            print(f"Random Private Key: {random_private_key}, Matching Address: {derived_address}")
+
 except KeyboardInterrupt:
     print("\nProgram interrupted by user.")
